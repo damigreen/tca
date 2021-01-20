200218336025535887ffd04e544de12be37c828c
Damilola Faseun <damilola.faseun@gmail.com>
2021-01-20 21:36:16 +0100

    init commit

---
+++ src/components/underconstruction/UnderConstruction.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class UnderConstruction extends Component {
+    
+    render() {
+
+        return(
+            <div>
+                <h1>Under Construction</h1>
+            </div>
+        )
+    }
+}
