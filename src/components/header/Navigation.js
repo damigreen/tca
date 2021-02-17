b396b38f2f34e8cebf03f6e30661dc9044828eff
Damilola <damilola.faseun@gmail.com>
2021-02-17 14:49:56 -0800

    Linked bootstrap to navigation

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const Navigation = () => {
 
     return (
         <div>
-            <ul>
+            {/* <ul>
                 <li>Home</li>
                 <li>About</li>
-                <li>Services</li>
                 <li>Work</li>
+                <li>Services</li>
+                <li>Careers</li>
                 <li>Contact</li>
-            </ul>
+            </ul> */}
+
+            <Nav>
+                <Nav.Item>
+                    <Nav.Link>About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>Services</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>Work</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>Careers</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>Contact</Nav.Link>
+                </Nav.Item>
+            </Nav>
         </div>
     )
 }
