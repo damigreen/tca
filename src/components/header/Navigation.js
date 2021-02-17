6d1f94e4fa72c8783aa4d85098078cb558ed1b22
Damilola <damilola.faseun@gmail.com>
2021-02-16 22:05:54 -0800

    Added navigation on application

---
+++ src/components/header/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation;
