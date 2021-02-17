a6c36f6de7b785da88e92afd30fc6890591b6cd3
Damilola <damilola.faseun@gmail.com>
2021-02-17 14:49:29 -0800

    created a test component

---
+++ src/components/testComponent/TestComponent.js
@@ -0,0 +1,32 @@
+import react, { Component } from 'react';
+import styled from 'styled-components';
+import Row from 'react-bootstrap/Row';
+
+
+class TestComponent extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h1>TEST</h1>
+                <Row>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                <div class="col-md-1">.col-md-1</div>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default TestComponent;
