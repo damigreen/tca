d06cfd783bc13d69db9d3bd093ea746eb141f519
Damilola <damilola.faseun@gmail.com>
2021-02-18 19:43:36 -0800

    floated the app logo

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import styled from 'styled-components';
+
+const NavWrap = styled.div`
+    background-color: pink;
+`
+
+const LogoBox = styled.div`
+    background-color: green;
+    float: left;
+`
+
 
 const Navigation = () => {
 
     return (
-        <div>
-            {/* <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Work</li>
-                <li>Services</li>
-                <li>Careers</li>
-                <li>Contact</li>
-            </ul> */}
+            <NavWrap>
+                <LogoBox>
+                    <img src />LOGO
+                </LogoBox>
 
-            <Nav className="justify-content-end" defaultActiveKey as="ul">
-                <Nav.Item>
-                    <Nav.Link href="/home">
-                        <img src />LOGO
-                    </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link  href="/about">About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="services">Services</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="work">Work</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="careers">Careers</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="contact">Contact</Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </div>
+                <Nav className="justify-content-end" defaultActiveKey as="ul">
+                    <Nav.Item>
+                        <Nav.Link  href="/about">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="services">Services</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="work">Work</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="careers">Careers</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="contact">Contact</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+
+            </NavWrap>
     )
 }
 
