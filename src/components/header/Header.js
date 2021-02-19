bb2134d3f1b46e65fae5ff2b2cb2cd519e7b61f7
Damilola <damilola.faseun@gmail.com>
2021-02-18 19:43:13 -0800

    added a headere wrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navigation from './Navigation';
+import styled from 'styled-components';
 
+const HeaderWrap = styled.div`
+  height: 700px;
+  max-height: 768px;
+  background-color: grey;
+`
 
 
 function Header() {
   return (
-    <div>
+    <HeaderWrap>
       <Navigation />
       <h1>Header</h1>
-    </div>
+    </HeaderWrap>
   );
 }
 
