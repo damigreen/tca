8e616aa9d2edefbbf3acecbe5acecee898cfc676
Damilola <damilola.faseun@gmail.com>
2021-02-25 22:55:11 +0100

    wrapped components on application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from './components/header/';
 import Container from 'react-bootstrap/Container';
+import Services from './components/services/';
+import About from './components/about/';
 import TestComponent from './components/testComponent/TestComponent';
 import styled from 'styled-components';
+
 
 const AppWrapper = styled.div`
   /* background-color: blue; */
   display: block;
+`
+
+const MainWrapper = styled.main`
+  border: 1px solid red;
+  text-align: center;
+
 `
 
 
@@ -16,8 +25,12 @@ function App() {
         <header>
           <Header />
         </header>
+        <MainWrapper>
+          <section>
+            <About />
+          </section>
+        </MainWrapper>
       <Container>
-        
         <TestComponent />
       </Container>
     </AppWrapper>
