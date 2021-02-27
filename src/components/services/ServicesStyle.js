f6cc9ae7f84a6d0452bcfd718f64e487145e28e1
Damilola <damilola.faseun@gmail.com>
2021-02-27 18:55:10 +0100

    styled component file for services

---
+++ src/components/services/ServicesStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Styled_ServicesWrap = styled.div`
+    border: 1px solid green;
+    /* display: inline-block; */
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`
+export const ServicesBox = styled.div`
+    border: 1px solid red;
+`
+
+export const ServicesHeader = styled.h4`
+    border: 1px solid green;
+    text-align: left;
+    /* padding: 0 0 5px; */
+    padding: 5px;
+    font-weight: 700;
+    color: #03738C;
+`
