105f869cefe41249b6084be618b03df90c5af4ff
Damilola <damilola.faseun@gmail.com>
2021-02-28 08:15:00 +0100

    Added a style module for the about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const AboutSection = styled.div`
-  border: 1px solid green;
-`
+import {
+  AboutSection,
+  AboutHeader,
+  HeaderIntro,
+} from './AboutStyles';
 
-const AboutHeader = styled.div`
-  border: 1px solid brown;
-`
-
-const HeaderIntro = styled.div`
-  border: 1px solid black;
-  background-color: green;
-`
 
 
 const About = () => {
---
+++ src/components/about/AboutStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const AboutSection = styled.div`
+  border: 1px solid green;
+`
+
+export const AboutHeader = styled.div`
+  border: 1px solid brown;
+`
+
+export const HeaderIntro = styled.div`
+  border: 1px solid black;
+  background-color: green;
+`
+
