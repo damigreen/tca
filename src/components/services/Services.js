fd7fbd3df66af182ece3de392e0c9ad24b80376d
Damilola <damilola.faseun@gmail.com>
2021-03-09 22:29:21 +0100

    Modified styled components name for services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,72 +1,79 @@
 import React from "react";
 // import "./Services.sass";
 import { 
-    Styled_ServicesWrap,
-    ServicesBox,
-    ServicesHeader,
+    ServicesWrap,
     ServicesIntro,
 
 } from './ServicesStyle'
 
 const Services = () => {
+    const sampleImage = '';
 
     return (
         <div>
-            {/* <h1>Services</h1> */}
-            <Styled_ServicesWrap>
+            <ServicesWrap>
+                <ServicesHeaderWrap>
+                    <ServicesIntroMain>
+                        Discover and Engage More Audience 
+                    </ServicesIntroMain>
 
-                <ServicesIntro>
-                    The Creative Agency equip your business with the right digital solutions and help you to achieve your goals
-                </ServicesIntro>
-                <ServicesBox>
-                    <ServicesHeader>Branding</ServicesHeader>
-                    <div>
-                        Our branding service helps handle all the hassle in-between transforming an idea into a scalable product then getting it to the marketplace to make profit.
-                        <img alt="services image for branding" />
-                    </div>
-                </ServicesBox>
+                    <ServicesIntro>
+                        We help fast-track businesses to their digital goals through unique branding, marketing and advertising.
+                    </ServicesIntro>
+                </ServicesHeaderWrap>
 
-                <ServicesBox>
-                    <ServicesHeader>Web Design and Development</ServicesHeader>
-                    <div>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        <img alt="services image for web design and development" />
-                    </div>
-                </ServicesBox>
+                <ServicesBodyWrap>
+
+                    <BrandingBox>
+                        <BrandingHeader>Branding</BrandingHeader>
+                        <div>
+                            Our branding service helps handle all the hassle in-between transforming an idea into a scalable product then getting it to the marketplace to make profit.
+                            <img alt="Branding image for branding" />
+                        </div>
+                    </BrandingBox>
+
+                    <WebDevBox className="web-dev">
+                        <WebDevHeader>Web Design and Development</WebDevHeader>
+                        <div>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            <img alt="WebDev image for web design and development" />
+                        </div>
+                    </WebDevBox>
 
 
-                <ServicesBox>
-                    <ServicesHeader>Consultancy</ServicesHeader>
-                    <div>
-                        An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.
-                        <img alt="services image for consultancy"  />
-                    </div>
-                </ServicesBox>
+                    <ConsultBox>
+                        <ConsultHeader>Consultancy</ConsultHeader>
+                        <div>
+                            An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.
+                            <img alt="Consult image for consultancy"  />
+                        </div>
+                    </ConsultBox>
 
-                <ServicesBox>
-                    <ServicesHeader>Scalability</ServicesHeader>
-                    <div>
-                        We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.
-                        <img alt="services image for scalability" />
-                    </div>
-                </ServicesBox>
+                    <ScalabilityBox>
+                        <ScalabilityHeader>Scalability</ScalabilityHeader>
+                        <div>
+                            We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.
+                            <img alt="Scalability image for scalability" />
+                        </div>
+                    </ScalabilityBox>
 
-                <ServicesBox>
-                    <ServicesHeader>Digital Marketing</ServicesHeader>
-                    <div>
-                        We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.
-                        <img alt="services image for digital marketing" />
-                    </div>
-                </ServicesBox>
+                    <DigitalMarketingBox>
+                        <DigitalMarketingHeader>Digital Marketing</DigitalMarketingHeader>
+                        <div>
+                            We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.
+                            <img alt="DigitalMarketing image for digital marketing" />
+                        </div>
+                    </DigitalMarketingBox>
 
-                <ServicesBox>
-                    <ServicesHeader>Publishing</ServicesHeader>
-                    <div>
-                        We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
-                        <img alt="services image for publishing" />
-                    </div>
-                </ServicesBox>
-            </Styled_ServicesWrap>
+                    <PublishingBox>
+                        <PublishingHeader>Publishing</PublishingHeader>
+                        <div>
+                            We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
+                            <img alt="Publishing image for publishing" />
+                        </div>
+                    </PublishingBox>
+                </ServicesBodyWrap>
+            </ServicesWrap>
         </div>
     )
 }
