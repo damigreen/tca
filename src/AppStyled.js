3efdc89d473c3fd375079932763679a29c1144d4
Damilola <damilola.faseun@gmail.com>
2021-03-10 09:40:38 +0100

    Applied default application style

---
+++ src/AppStyled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.h1`
+    font-size: 24px;
+    font-weight: 600;   
+    text-align: center;
+`
+
+export const FlexWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const BorderStyle = styled.div`
+    border: 2px solid black;
+`
