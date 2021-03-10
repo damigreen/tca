95ef80aaa3168af6d1adafba506bd22742a6690a
Damilola <damilola.faseun@gmail.com>
2021-03-10 23:22:32 +0100

    mockup the service layout

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,22 +1,15 @@
+import Header from "components/header";
 import React from "react";
 // import "./Services.sass";
 import { 
     ServicesWrap,
     ServicesRow,
     BrandingBox,
-    // BrandingImage,
-    BrandingHeader,
-    BrandingBody,
     WebDevBox,
-    WebDevHeader,
     ConsultBox,
-    ConsultHeader,
     ScalabilityBox,
-    ScalabilityHeader,
     DigitalMarketingBox,
-    DigitalMarketingHeader,
     PublishingBox,
-    PublishingHeader,
 } from './ServicesStyle'
 
 const Services = () => {
@@ -37,7 +30,6 @@ const Services = () => {
                     </BrandingBox>
 
                     <WebDevBox>
-
                         <div className="services-body">
                             <h4>Web Design and Development</h4>
                             <div>
@@ -50,33 +42,49 @@ const Services = () => {
                     </WebDevBox>
 
                     <ConsultBox>
-                        <ConsultHeader>Consultancy</ConsultHeader>
-                        <div>
-                            An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.
+                        <div className="services-image">
                             <img alt="Consult image for consultancy"  />
+                        </div>
+                        <div className="services-body">
+                            <h4>Consultancy</h4>
+                            An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.
                         </div>
                     </ConsultBox>
 
                     <ScalabilityBox>
-                        <ScalabilityHeader>Scalability</ScalabilityHeader>
-                        <div>
-                            We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.
+                        <div className="services-body">
+                            <h4>Scalability</h4>
+                            <div>
+                                We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.
+                            </div>
+
+                        </div>
+                        <div className="services-image">
                             <img alt="Scalability image for scalability" />
                         </div>
                     </ScalabilityBox>
 
                     <DigitalMarketingBox>
-                        <DigitalMarketingHeader>Digital Marketing</DigitalMarketingHeader>
-                        <div>
-                            We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.
+                        <div className="services-image">
                             <img alt="DigitalMarketing image for digital marketing" />
+                        </div>
+
+                        <div className="services-body">
+                            <h4>Digital Marketing</h4>
+                            <div>
+                                We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.
+                            </div>
                         </div>
                     </DigitalMarketingBox>
 
                     <PublishingBox>
-                        <PublishingHeader>Publishing</PublishingHeader>
-                        <div>
-                            We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
+                        <div className="services-body">
+                            <h4>Publishing</h4>
+                            <div>
+                                We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
+                            </div>
+                        </div>
+                        <div className="services-image">
                             <img alt="Publishing image for publishing" />
                         </div>
                     </PublishingBox>
