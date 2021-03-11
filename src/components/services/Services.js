32594440bfce722c10d2eb5bf918cc4a29346c60
Damilola <damilola.faseun@gmail.com>
2021-03-11 21:09:24 +0100

    added a call to action button son services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -12,6 +12,10 @@ import {
     PublishingBox,
 } from './ServicesStyle'
 
+import {
+    ButtonServices
+} from 'AppStyled';
+
 const Services = () => {
 
     return (
@@ -26,6 +30,7 @@ const Services = () => {
                         <div className="services-body">
                             <h4>Branding</h4>
                             <div>Our branding service helps handle all the hassle in-between transforming an idea into a scalable product then getting it to the marketplace to make profit.</div>
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                     </BrandingBox>
 
@@ -35,6 +40,7 @@ const Services = () => {
                             <div>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </div>
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                         <div className="services-image">
                             <img alt="WebDev image for web design and development" />
@@ -47,7 +53,8 @@ const Services = () => {
                         </div>
                         <div className="services-body">
                             <h4>Consultancy</h4>
-                            An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.
+                            <div>An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.</div>
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                     </ConsultBox>
 
@@ -57,7 +64,7 @@ const Services = () => {
                             <div>
                                 We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.
                             </div>
-
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                         <div className="services-image">
                             <img alt="Scalability image for scalability" />
@@ -74,6 +81,7 @@ const Services = () => {
                             <div>
                                 We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.
                             </div>
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                     </DigitalMarketingBox>
 
@@ -83,6 +91,7 @@ const Services = () => {
                             <div>
                                 We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
                             </div>
+                            <ButtonServices>LEARN MORE</ButtonServices>
                         </div>
                         <div className="services-image">
                             <img alt="Publishing image for publishing" />
