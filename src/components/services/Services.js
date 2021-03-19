66cd575675e1eb9065bec4ba81dc9ac7e624b362
Damilola <damilola.faseun@gmail.com>
2021-03-19 09:10:41 +0100

    refactor component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -141,7 +141,6 @@ const Services = () => {
                                     <svg style={{width: '100', marginLeft: 10}} xmlns="http://www.w3.org/2000/svg" width="100" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 100 16">
                                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                                     </svg>
-                                    {/* <img src={rightArrow} alt="Learn more right arrow" /> */}
                                 </span>
                             </p>
                         </div>
