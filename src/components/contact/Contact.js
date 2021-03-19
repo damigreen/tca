3542bb4f6a5b91f4c0dbf0d18e4e40cc060771c1
Damilola <damilola.faseun@gmail.com>
2021-03-19 09:53:50 +0100

    Added contact field to contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import useForm from 'hooks/formHook';
 
 const Contact = () => {
+    const fullName = useForm('text');
+    const email = useForm('email');
+
 
     return (
         <div>
             <h1>Contact Us</h1>
             <div>
                 <form>
-                    <label for="name">Name</label>
-                    <input type="text" name="" />
+                    <label for="name"></label>
+                    <input placeholder="Full Name" { ...fullName.form} />
+
+                    <label for="name"></label>
+                    <input placeholder="email" { ...email.form} />
+
+                    <label for="phone"></label>
+                    <input placeholder="Phone" { ...fullName.form} />
+
+                    <label for="company name"></label>
+                    <input placeholder="company name" { ...fullName.form} />
+
+                    <label for="message"></label>
+                    <input placeholder="Tell us your request" { ...fullName.form} />
                 </form>
             </div>
         </div>
