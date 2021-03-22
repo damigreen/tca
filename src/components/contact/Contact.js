f9851cde321bf5281e5265c56ac5e4a57946ddd9
Damilola <damilola.faseun@gmail.com>
2021-03-22 22:55:24 +0100

    Added new contact layout

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import useForm from 'hooks/formHook';
+import {
+    ContactWrapper,
+    ContactRow,
+
+} from './ContactStyled';
+
+
 
 const Contact = () => {
     const fullName = useForm('text');
@@ -7,27 +14,30 @@ const Contact = () => {
 
 
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <div>
-                <form>
-                    <label for="name"></label>
-                    <input placeholder="Full Name" { ...fullName.form} />
+        <ContactWrapper>
+            <ContactRow>
+                <h1>Contact Us</h1>
+                <div>
+                    <form>
+                        <label for="name"></label>
+                        <input placeholder="Full Name" { ...fullName.form} />
 
-                    <label for="name"></label>
-                    <input placeholder="email" { ...email.form} />
+                        <label for="name"></label>
+                        <input placeholder="email" { ...email.form} />
 
-                    <label for="phone"></label>
-                    <input placeholder="Phone" { ...fullName.form} />
+                        <label for="phone"></label>
+                        <input placeholder="Phone" { ...fullName.form} />
 
-                    <label for="company name"></label>
-                    <input placeholder="company name" { ...fullName.form} />
+                        <label for="company name"></label>
+                        <input placeholder="company name" { ...fullName.form} />
 
-                    <label for="message"></label>
-                    <input placeholder="Tell us your request" { ...fullName.form} />
-                </form>
-            </div>
-        </div>
+                        <label for="message"></label>
+                        <input placeholder="Tell us your request" { ...fullName.form} />
+                    </form>
+                </div>
+
+            </ContactRow>
+        </ContactWrapper>
     )
 }
 
---
+++ src/components/contact/ContactStyled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ContactWrapper = styled.div`
+    border: 1px solid red;
+`
+export const ContactRow = styled.div`
+    border: 1px solid red;
+`
