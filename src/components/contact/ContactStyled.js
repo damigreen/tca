d72fc87c82e545c54fcd948785a8cb569951448f
Damilola <damilola.faseun@gmail.com>
2021-03-23 21:55:42 +0100

    modified form layout

--- src/components/contact/ContactStyled.js
+++ src/components/contact/ContactStyled.js
@@ -1,8 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+
 
 export const ContactWrapper = styled.div`
-    border: 1px solid red;
+    /* background-color: #011b32; */
+    /* background-color: #d3d3d3; */
+    border-top: 1px solid gray;
 `
 export const ContactRow = styled.div`
-    border: 1px solid red;
+    text-align: left;
+    padding:50px;
+
+    h1 {
+        text-align: center;
+    }
+
+    .form-group {
+        display: flex;
+    }
+
+    /* .form-name {
+        background: url('../../../icons/person.svg');
+        ${backgroundFormStyle};
+    } */
+
+    .form-email {
+    }
+
+    .form-phone {
+    }
+
+    .form-company {
+    }
+
+    .form-message {
+    }
 `
