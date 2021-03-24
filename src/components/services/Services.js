9acbb688c33402b6d41d956074c3012ad621fb6f
Damilola <damilola.faseun@gmail.com>
2021-03-24 19:03:44 +0100

    removed srvices second heading

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -39,7 +39,7 @@ const Services = () => {
 
                         <div className="services-body">
                             <h4>Branding</h4>
-                            <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                            {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                             <div>Our branding service helps handle all the hassle in-between transforming an idea into a scalable product then getting it to the marketplace to make profit.</div>
                             <p className="btn-services">Learn more
                                 <span>
@@ -63,7 +63,7 @@ const Services = () => {
 
                         <div className="services-body">
                             <h4>Web Design and Development</h4>
-                            <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                            {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                             <div>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </div>
@@ -86,7 +86,7 @@ const Services = () => {
                         </div>
                         <div className="services-body">
                             <h4>Consultancy</h4>
-                            <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                            {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                             <div>An appointment for our consultancy service provides clients with timely professional advice to all the questions they need answered regarding all aspects of their business.</div>
                             {/* <ButtonServices>LEARN MORE</ButtonServices> */}
                             <p className="btn-services">Learn more
@@ -108,7 +108,7 @@ const Services = () => {
                         <div className="services-body">
                             <div>
                                 <h4>Scalability</h4>
-                                <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                                {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                                 <div>We help scale your brand to break-even and make revenue. We take up the hassle of strategizing and positioning the brand to make profit.</div>
                             </div>
                             {/* <ButtonServices>LEARN MORE</ButtonServices> */}
@@ -132,7 +132,7 @@ const Services = () => {
                         <div className="services-body">
                             <div>
                                 <h4>Digital Marketing</h4>
-                                <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                                {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                                 <div>We assists clients in determining their strengths, weaknesses and uniqueness as a brand. The agency offers innovative & creative digital marketing methods to target audiences for already established brands/clients as well as brands/clients just starting out in their various fields.</div>
                             </div>
                             {/* <ButtonServices>LEARN MORE</ButtonServices> */}
@@ -154,7 +154,7 @@ const Services = () => {
 
                         <div className="services-body">
                             <h4>Publishing</h4>
-                            <h3>Lorem Ipsum is simply dummy text of the printing</h3>
+                            {/* <h3>Lorem Ipsum is simply dummy text of the printing</h3> */}
                             <div>
                                 We write reviews for products, brands and individual clients to be published on our platform’s blog or other respected media outlets eg: Medium, Native Mag or the client’s choice of media house.
                             </div>
