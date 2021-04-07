d16245363c82c8403359af7b366393e09c8d18db
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-07 13:02:11 +0100

    changed navigation layout

--- src/components/header/HeaderStyled.js
+++ src/components/header/HeaderStyled.js
@@ -2,20 +2,39 @@ import styled from 'styled-components';
 
 
 export const HeaderWrap = styled.div`
-  height: 700px;
-  max-height: 768px;
-  background: url('../../../images/hero-image.png');
-  background-repeat: no-repeat;
-  background-size: 700px 500px
+    max-height: none;
+    z-index: -82929292;
+    clear: both;
+
+    .hero {
+      max-width: 100%;
+      height: auto;
+      border: 0;
+    }
 `
 
-export const HeroImage = styled.div`
-
-`
 export const NavWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: nowrap;
+  margin: 0 auto;
+  height: 11vh;
+  width: 84%;
+  border: 1px solid red;
+
+  .nav-item {
+    display: none;
+  }
+
+  .nav-filter {
+
+  }
 `
 
 export const LogoBox = styled.div`
-      background-color: green;
+    height: 52;
+    width: 52;
     float: left;
 `
--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -7,16 +7,25 @@ import {
     LogoBox,
 } from './HeaderStyled';
 
+const logoImage = '../../../icons/footer-logo-icon.png'
+
 
 const Navigation = () => {
 
     return (
             <NavWrap>
                 <LogoBox>
-                    <img src />LOGO
+                    <img src={logoImage} />
                 </LogoBox>
 
-                <Nav className="justify-content-end" defaultActiveKey as="ul">
+
+                <div className="nav-filter">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
+                        <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </div>
+
+                <Nav className="nav-item justify-content-end" defaultActiveKey as="ul">
                     <Nav.Item>
                         <Nav.Link  href="/about">About</Nav.Link>
                     </Nav.Item>
@@ -26,18 +35,15 @@ const Navigation = () => {
                     <Nav.Item>
                         <Nav.Link eventKey="work">Work</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    {/* <Nav.Item>
                         <Nav.Link eventKey="careers">Careers</Nav.Link>
-                    </Nav.Item>
+                    </Nav.Item> */}
                     <Nav.Item>
                         <Nav.Link eventKey="contact">Contact</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>  
                         <Nav.Link eventKey="login-form">Login</Nav.Link>
                     </Nav.Item>
-                    {/* <Nav.Item>
-                        <Nav.Link eventKey="login-form">Login</Nav.Link>
-                    </Nav.Item> */}
                 </Nav>
 
             </NavWrap>
