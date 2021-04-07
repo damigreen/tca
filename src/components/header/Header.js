13421fe25e8432451c79e6b983e62303a902d925
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-07 13:01:52 +0100

    modified layout for hero image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,20 +2,19 @@ import React from 'react';
 import Navigation from './Navigation';
 import {
   HeaderWrap,
-  HeroImage
+  HeroImage,
 } from './HeaderStyled';
 
-
-// const heroImage = '../../../images/hero-image.png';
+const heroImage = '../../../images/hero-image.png';
 
 
 function Header() {
   return (
     <HeaderWrap>
-      <HeroImage>
-        {/* <img className="hero- image" src={heroImage} alt="Hero image for the header" /> */}
         <Navigation />
-      </HeroImage>
+        <div className="hero-image">
+          <img className="hero" src={heroImage} />
+        </div>
     </HeaderWrap>
   );
 }
