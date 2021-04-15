84a6ad4d42ab17cf17d7ec53d915cd3e30b44035
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-15 09:47:04 -0500

    Added filter and use porftolio data from DB

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,90 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import {
   PortfolioWrapper,
   PortfolioRow,
 } from './StyledPortfolio';
 
 
-
 const Portfolio = () => {
-  const portfolioList = [
-    {
-      name: 'Team Bere',
-      description: '',
-      images: ['../projects/team-bere-mobile.png'],
-      categories:  ['branding', 'design', 'consultancy'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'All You Can Eat',
-      description: '',
-      images: ['../projects/all-you-can-eat-mobile.png'],
-      categories:  ['branding', 'portfolio', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Bola Catering Services',
-      description: '',
-      images: ['../projects/bola-catering-service-mobile.png'],
-      categories:  ['branding', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Mercy FC',
-      description: '',
-      images: ['../projects/mercyfc-mobile.png'],
-      categories: ['branding', 'consultancy', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Soccer Match',
-      description: '',
-      images: ['../projects/soccer-match-mobile.png'],
-      categories: ['consultancy', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Cupid Cakes',
-      description: '',
-      images: ['../projects/all-you-can-eat1-mobile.png'],
-      categories: ['branding', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'NHB',
-      description: '', 
-      images: ['../projects/nhb-mobile.png', '../projects/nhb1-mobile.png'],
-      categories: ['branding', 'consultancy'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Rayckan Herbal Healthcare',
-      description: '',
-      images: [],
-      categories: ['web development', 'branding', 'portfolio'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      name: 'Blog Keep',
-      description: '',
-      images: [],
-      categories: ['web development', 'branding', 'design'],
-      description: 'is simply dummy text of the printing and typesetting industry.'
-    },
-  ]
+  const [portfolioList, setPortfolioList] = useState([]);
 
-  const filterAll = () => {
-      portfolioList.map(port => {
-      const portCartegory = port.categories;
-      const brand = portCartegory.find(cat => cat === 'portfolio') ? port : 'yes';
-      console.log(brand);
+  const portfolioUrl = "../../../portfolioData.json"
 
-      return (
-        <div>
-          {brand}
-        </div>
-      )
-    })    
+  useEffect(() => {
+    axios.get(portfolioUrl)
+      .then(result => {
+        setPortfolioList(result.data)
+        console.log(result);
+      });
+  }, []);
+  
+
+
+  const filterAll = (cat) => {
+      setPortfolioList(portfolioList);
+      let filteredList = [];
+
+      for (var i = 0; i < portfolioList.length; i++) {
+        const projectCategories = portfolioList[i].categories;
+        const catLen = projectCategories.length;
+        
+        for (var j = 0; j < catLen; j++) {
+          // if (projectCategories[j] === 'branding') {
+            console.log(cat);
+            if (projectCategories[j] === cat) {
+              filteredList.push(portfolioList[i]);
+              setPortfolioList(filteredList);
+          }
+        }
+      }
+      console.log(filteredList);
+
+    return 0;
   }
 
 
@@ -93,7 +49,6 @@ const Portfolio = () => {
       const projectName = port.name;
       const projectDesc = port.description;
       const projectImage = port.images;
-      console.log(projectImage)
 
         return (
           <article>
@@ -118,8 +73,8 @@ const Portfolio = () => {
         <div className="portfolio-content">
           <h1 className="portfolio-header">Our Projects</h1>
           <div className="portfolio-category">
-            <a className="portfolio-categrory active" href onClick={filterAll}>All</a>
-            <a className="portfolio-categrory" href onClick>Branding</a>
+            <a className="portfolio-categrory active" href onClick={() => filterAll('consultancy')}>All</a>
+            <a className="portfolio-categrory" href onClick={() => filterAll('branding')}>Branding</a>
             <a className="portfolio-categrory" href onClick>Web Development</a>
             <a className="portfolio-categrory" href onClick>Consultancy</a>
             <a className="portfolio-categrory" href onClick>Design</a>
