179fdd465d6c512b3d991cc926a3bac86f6222cc
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-15 15:20:56 -0500

    created logic to expand navigation in mobile first

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -5,14 +5,27 @@ import LoginForm from 'components/loginForm/';
 import {
     NavWrap,
     LogoBox,
+    TopBar
 } from './HeaderStyled';
 
 const logoImage = '../../../icons/footer-logo-icon.png'
 
 
+
+
 const Navigation = () => {
 
+    const navExpand = () => {
+        let navBar = document.querySelector('.item-bar');
+        if (navBar.style.display === 'block') {
+            navBar.style.display = 'none'
+        } else {
+            navBar.style.display = 'block';
+        }
+    }
+
     return (
+        <TopBar className="top-bar">
             <NavWrap>
                 <LogoBox>
                     <img src={logoImage} />
@@ -22,35 +35,41 @@ const Navigation = () => {
                     </div>
                 </LogoBox>
 
+                    <a className="nav-filter" onClick={navExpand}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
+                            <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                    </a>
 
-                <div className="nav-filter">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
-                        <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                </div>
 
-                <Nav className="nav-item justify-content-end" defaultActiveKey as="ul">
+
+            </NavWrap>
+
+            <div className="item-bar">
+                <Nav className="nav-item flex-column" variant="pills" defaultActiveKey as="ul">
                     <Nav.Item>
-                        <Nav.Link  href="/about">About</Nav.Link>
+                        <Nav.Link className="nav-link" href="/about">About</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="services">Services</Nav.Link>
+                        <Nav.Link className="nav-link" eventKey="services">Services</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="work">Work</Nav.Link>
+                        <Nav.Link className="nav-link" eventKey="work">Work</Nav.Link>
                     </Nav.Item>
                     {/* <Nav.Item>
                         <Nav.Link eventKey="careers">Careers</Nav.Link>
                     </Nav.Item> */}
                     <Nav.Item>
-                        <Nav.Link eventKey="contact">Contact</Nav.Link>
+                        <Nav.Link className="nav-link" eventKey="contact">Contact</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>  
-                        <Nav.Link eventKey="login-form">Login</Nav.Link>
+                        <Nav.Link className="nav-link" eventKey="login-form">Login</Nav.Link>
                     </Nav.Item>
                 </Nav>
 
-            </NavWrap>
+            </div>
+
+        </TopBar>
     )
 }
 
